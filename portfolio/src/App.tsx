d6323e8ac49981f33c8df95424ceb405112d191b
kevinesg <kevinesg.dev@gmail.com>
2025-09-06 02:27:04 +0800

    created placeholder navigation pages

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Experience from "./pages/Experience";
+import About from "./pages/About";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
