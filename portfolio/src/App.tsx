346b1f03cd87c4e80bc910c731305f73d012b3d9
kevinesg <kevinesg.dev@gmail.com>
2025-09-06 12:17:46 +0800

    added page routing

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Experience from "./pages/Experience";
+import About from "./pages/About";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <div className="mx-auto max-w-[800px] px-4 sm:px-6 lg:px-8">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
