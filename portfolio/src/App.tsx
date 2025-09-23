0b16936b3f890a9dbc664c28bfa48ec425bee3b1
kevinesg <kevinesg.dev@gmail.com>
2025-09-24 02:51:46 +0800

    implemented page routing

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Navbar from "@/components/Navbar";
+import Home from "@/pages/Home";
+import Experience from "@/pages/Experience";
+import About from "@/pages/About";
+import Footer from "@/components/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
